372060be457083b11221ed1141b46eba78a713d6
letuan4388 <letuan4388@gmail.com>
2017-05-08 22:56:09 +0700

    Add files via upload

---
+++ project_ajax.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+var strGPIO1 = "";
+function GetGPIO() {
+	nocache = "&nocache= " + Math.random();
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			ProcessResponse(this);
+			GetGPIO();
+		}
+	};
+	xhttp.open("GET", strGPIO1 + nocache, true);
+	xhttp.send();
+	strGPIO1 = "";
+}
+
+function ProcessResponse(xml) {
+	var x = xml.responseXML.getElementsByTagName("GPIO");
+	document.getElementById("gpio1").innerHTML = x[0].childNodes[0].nodeValue;
+}
+
+function SetGPIO() {
+	 strGPIO1 = "GPIO1";
+}
